bb3906b5ad995ea81b535c93a7515e95be3e01b4
WangFanhua <18915636179@163.com>
2017-05-20 17:38:09 +0800

    热加载

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+    <App />, 
+    document.getElementById('root')
+);
+// 热加载启动
+if (module.hot) {
+    module.hot.accept();
+}
+// registerServiceWorker();
